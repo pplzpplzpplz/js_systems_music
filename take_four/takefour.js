6585dde924f617561a559ec0ce0bd3484485b367
pplzpplzpplz <moore.caleb@gmail.com>
2021-08-22 20:44:16 -0700

    ehh

--- take_four/takefour.js
+++ take_four/takefour.js
@@ -19,55 +19,54 @@ playpauseButton.addEventListener('click', (e)=>{
 const stopButton = document.querySelector('.stop');
 stopButton.addEventListener('click', stopIt);
 
+const progressBar = document.querySelector('.progressbar');
+
+
 
 // AUDIO STUFF 
 let audioContext = new AudioContext();
+const audioOne = document.querySelector('audio');
+
 
 
 function startIt() {
   audioContext.resume();
 
-  // LOOP THROUGH 1-4
-  for (var i = 1; i < 5; i++) {
+    let positionDiv = document.getElementsByClassName(`p1`); // grab each position line div
 
-    
-    let positionDiv = document.getElementsByClassName(`p${i}`); // grab each position line div
-
-    let audioLength = 57.6; // audio loop lengths in seconds
-    let pxWidth = 336;     // width of player container divs
+    let audioLength = audioOne.duration; // audio loop lengths in seconds
+    let pxWidth = progressBar.offsetWidth; // width of player container divs   
     let offsetAmount = Math.random() * audioLength;  // random starting time for each audio
     let percentageComplete = (offsetAmount / audioLength); // percentage of audio loop completed AT START
     let pixelsCompleteOfDiv = Math.floor(percentageComplete * pxWidth); // pixel position of position line div AT START
     
 
-    function startLoop(audioBuffer, pan = 0, rate = 1) {
-      i = i + 1;
-
-
-      
+    function startLoop(audioBuffer, pan = 0, rate = 1) {      
       
       if (playpauseButton.classList.contains('pause') && (counter < 2)) {
-        console.log(`Audio ${i-5} offset amount: ${offsetAmount} seconds`);
-        console.log(`Audio ${i-5} pixels: ${pixelsCompleteOfDiv}px`);
 
-        // every half second, change the position divs to match the currentTime
-        setInterval(updatePositionDiv, 500);
-        function updatePositionDiv() {
-          let positionDivPixels = (((audioContext.currentTime + offsetAmount) / audioLength) * pxWidth);
-          if (positionDivPixels > pxWidth) {
-            // reset back to 0 position 
-            positionDiv[0].style.left = `0px`;
-            // get the new pixel position of the div
-            positionDivPixels = positionDivPixels - pxWidth;
-            positionDiv[0].style.left = `${positionDivPixels}px`;
-            positionDivPixels = 0;
-            return;
-          } else {
-          positionDiv[0].style.left = `${positionDivPixels}px`;
-          return;
-        }
-          
-        }
+        console.log(`Audio 1 offset amount: ${offsetAmount} seconds`);
+        console.log(`Audio 1 pixels: ${pixelsCompleteOfDiv}px`);
+
+
+        
+        // function updatePositionDiv() {
+        //   let positionDivPixels = (((audioContext.currentTime + offsetAmount) / audioLength) * pxWidth);
+        //   if (positionDivPixels > pxWidth) {
+        //     // reset back to 0 position 
+        //     positionDiv[0].style.left = `0px`;
+        //     // get the new pixel position of the div
+        //     positionDivPixels = positionDivPixels - pxWidth;
+        //     positionDiv[0].style.left = `${positionDivPixels}px`;
+        //     positionDivPixels = 0;
+        //     return;
+        //   } else {
+        //   positionDiv[0].style.left = `${positionDivPixels}px`;
+        //   return;
+        //   }
+        // }
+        // updatePositionDiv();
+
       } else {
         return;}
 
@@ -95,20 +94,28 @@ function startIt() {
       pannerNode.connect(gainNode);
       gainNode.connect(audioContext.destination);
       sourceNode.start(0, offsetAmount);  
+      sourceNode.addEventListener('timeupdate', () => {
+        console.log('The currentTime attribute has been updated. Again.');
+      })
     }
 
-    fetch(`${i}.wav`) 
+    
+
+
+    fetch(`1.wav`) 
       .then(response => response.arrayBuffer()) 
       .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) 
       .then(audioBuffer => {
         startLoop(audioBuffer, 0, 4);
       })
       .catch(error => console.error(error));
-  }
+
   
 }
 
-
+// audioOne.addEventListener('timeupdate', (event) => {
+//   console.log('The currentTime attribute has been updated. Again.');
+// });
 
   // PAUSE MUSIC
   function pauseIt() {
--- take_four/takefour_restart.js
+++ take_four/takefour_restart.js
@@ -5,47 +5,55 @@ const progressBar = document.querySelector('.progressbar');
 let positionDiv = document.querySelector('.p1'); // grab the position line div
 
 
-const audioOne = document.querySelector('audio');
+
 
 let counter = 0;
 
-let audioLength = audioOne.duration; // audio loop lengths in seconds
 let pxWidth = progressBar.offsetWidth; // width of player container divs   
+
+
+// web audio api 
+// const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
+const audioOne = document.querySelector('audio');
+
+let audioLength = audioOne.duration; // audio loop lengths in seconds
 let offsetAmount = Math.random() * audioLength;  // random starting time for each audio
 let percentageComplete = (offsetAmount / audioLength); // percentage of audio loop completed AT START
 let pixelsCompleteOfDiv = Math.floor(percentageComplete * pxWidth); // pixel position of position line div AT START
 
-playPauseButton.addEventListener('click', (e)=>{
-  counter++; // increment counter
-  playIt();
-  e.target.classList.toggle('pause'); // toggle pause class
-  if (!e.target.classList.contains('pause')) {
-    pauseIt();
+
+const audioOneTrack = audioContext.createMediaElementSource(audioOne);
+audioOneTrack.connect(audioContext.destination);
+
+// play / pause audio 
+playPauseButton.addEventListener('click', function() {
+
+  // check if context is in suspended state (autoplay policy)
+  if (audioContext.state === 'suspended') {
+      audioContext.resume();
   }
-})
+
+  // play or pause track depending on state
+  if (this.dataset.playing === 'false') {
+      audioContext.currentTime = offsetAmount;
+      audioOne.play();
+      this.dataset.playing = 'true';
+  } else if (this.dataset.playing === 'true') {
+      audioOne.pause();
+      this.dataset.playing = 'false';
+  }
+
+}, false);
 
 
 
 
 // STOP BUTTON 
-
 stopButton.addEventListener('click', stopIt);
 audioOne.loop = true;
 
-
-// PLAY MUSIC
-function playIt() {
-  audioOne.currentTime = offsetAmount; // set audio current time to offset amount
-  audioOne.play();
-  
-}
-
-// PAUSE MUSIC
-function pauseIt() {
-  audioOne.pause();
-}
-
-// STOP MUSIC
 function stopIt() {
   audioOne.pause();
   audioOne.currentTime = 0;
