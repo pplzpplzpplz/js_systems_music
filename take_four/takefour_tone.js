b613de771de1886c830e145ca53d95647383abc9
pplzpplzpplz <moore.caleb@gmail.com>
2021-09-03 16:20:05 -0700

    kinda got it..

--- take_four/takefour_tone.js
+++ take_four/takefour_tone.js
@@ -1,6 +1,8 @@
 let audioContext = new AudioContext();
 const playStopButton = document.querySelector('.playstop');
 const currentTimeDiv = document.querySelector('.currentTime');
+const p1 = document.querySelector('.p1');
+const b1 = document.querySelector('.b1');
 
 
 playStopButton.addEventListener('click', function() {
@@ -14,6 +16,7 @@ playStopButton.addEventListener('click', function() {
       // STOP MUSIC
       player.stop();
       Tone.Transport.stop();
+      player.seek(0);
       console.log('stopped');
       this.dataset.playing = 'false';
   }
@@ -45,22 +48,46 @@ function startIt() {
     console.log(`randomStartPosition: ${randomStartPosition}`);
     console.log(`Tone.Transport.seconds: ${Tone.Transport.seconds + randomStartPosition}`);
 
+    let currentPosition = Tone.Transport.seconds + randomStartPosition;
+    let playCount = 1;
 
 
     setInterval(function() {
-      let currentPosition = Tone.Transport.seconds;
+      // song is less than one duration
+      if (currentPosition < buffer.duration) {
 
-      if (currentPosition > buffer.duration) {
+        currentPosition = Tone.Transport.seconds + randomStartPosition;
+      } 
+              // song is more than one duration
+      else if (currentPosition >= buffer.duration) {
+
+        playCount = playCount + 1;
+        randomStartPosition = 0;
         currentPosition = 0;
+        Tone.Transport.seconds = 0;
       }
+
+      p1.style.left = `${currentPosition / p1.offsetWidth}%`;
+      
+      // if (currentPosition > buffer.duration) {
+      //   console.log('reached end of track');
+      //   currentPosition = 0;
+      //   Tone.Transport.seconds = 0;
+      //   currentPosition = Tone.Transport.seconds;
+      // } else {
+      //   console.log('else triggered');
+      //   currentPosition = Tone.Transport.seconds;
+      // }
 
       currentTimeDiv.innerHTML = `
       buffer.duration: ${buffer.duration} <br>
       randomStartPosition:  ${randomStartPosition} 
       <br> 
-      Tone.Transport.seconds: ${Math.floor(currentPosition) + randomStartPosition}
+      Tone.Transport.seconds: ${currentPosition.toFixed(1)}
+      <br>
+      playCount: ${playCount}
       `;
-    });
+    }), 1000;
   });
 
 
