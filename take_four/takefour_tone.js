b3ffd7782139ce9761df927e278523858b97ab71
pplzpplzpplz <moore.caleb@gmail.com>
2021-09-05 12:48:54 -0700

    player is working, playCount doesn't reset so using location.reload();

--- take_four/takefour_tone.js
+++ take_four/takefour_tone.js
@@ -1,8 +1,10 @@
 let audioContext = new AudioContext();
 const playStopButton = document.querySelector('.playstop');
-const currentTimeDiv = document.querySelector('.currentTime');
+const debugDiv = document.querySelector('.debugDiv');
 const p1 = document.querySelector('.p1');
 const b1 = document.querySelector('.b1');
+let playCount;
+
 
 
 playStopButton.addEventListener('click', function() {
@@ -14,18 +16,17 @@ playStopButton.addEventListener('click', function() {
 
   } else if (this.dataset.playing === 'true') {
       // STOP MUSIC
-      player.stop();
-      Tone.Transport.stop();
-      player.seek(0);
+      location.reload(); // cant get the playCount to reset, so using page reload here
+      stopIt();
       console.log('stopped');
       this.dataset.playing = 'false';
   }
 }, false);
 
-  // tone.js - new Buffer for audio file, to grab duration
-  var buffer = new Tone.Buffer("1.wav");
-  // tone.js - new Player for audio file
-  const player = new Tone.Player(buffer).toDestination();
+// tone.js - new Buffer for audio file, to grab duration
+var buffer = new Tone.Buffer("1b.wav");
+// tone.js - new Player 
+const player = new Tone.Player(buffer).toDestination();
 
 
 function startIt() {
@@ -33,10 +34,9 @@ function startIt() {
   Tone.loaded().then(() => {
 
     // pick a random start time within the duration of the audio file
-    let randomStartPosition = Math.floor(Math.random() * (buffer.duration));
+    randomStartPosition = Math.random() * (buffer.duration);
     
-
-    player.fadeIn = 4;
+    player.fadeIn = .1;
     player.loop = true;
     player.playbackRate = 1;
     player.start();
@@ -45,53 +45,47 @@ function startIt() {
     // seek to the random start position
     player.seek(randomStartPosition);
 
-    console.log(`randomStartPosition: ${randomStartPosition}`);
-    console.log(`Tone.Transport.seconds: ${Tone.Transport.seconds + randomStartPosition}`);
-
     let currentPosition = Tone.Transport.seconds + randomStartPosition;
-    let playCount = 1;
+    playCount = 1;
 
 
     setInterval(function() {
       // song is less than one duration
       if (currentPosition < buffer.duration) {
-
         currentPosition = Tone.Transport.seconds + randomStartPosition;
       } 
-              // song is more than one duration
-      else if (currentPosition >= buffer.duration) {
 
+      // song is more than one duration
+      else if (currentPosition >= buffer.duration) {
         playCount = playCount + 1;
         randomStartPosition = 0;
         currentPosition = 0;
         Tone.Transport.seconds = 0;
       }
 
-      p1.style.left = `${currentPosition / p1.offsetWidth}%`;
-      
-      // if (currentPosition > buffer.duration) {
-      //   console.log('reached end of track');
-      //   currentPosition = 0;
-      //   Tone.Transport.seconds = 0;
-      //   currentPosition = Tone.Transport.seconds;
-      // } else {
-      //   console.log('else triggered');
-      //   currentPosition = Tone.Transport.seconds;
-      // }
+      // move the line with the audio playback
+      p1.style.left = `${((currentPosition / buffer.duration) * 100)}%`;
 
-      currentTimeDiv.innerHTML = `
-      buffer.duration: ${buffer.duration} <br>
-      randomStartPosition:  ${randomStartPosition} 
+      debugDiv.innerHTML = `
+      randomStartPosition:  ${randomStartPosition.toFixed(1)}
       <br> 
-      Tone.Transport.seconds: ${currentPosition.toFixed(1)}
+      buffer.duration: ${buffer.duration} <br>
+      currentPosition: ${Math.floor(currentPosition)}
       <br>
       playCount: ${playCount}
+      <br>
       `;
     }), 1000;
   });
+}
 
-
-
+function stopIt() {
+  playCount = 0;
+  player.stop();
+  Tone.Transport.stop();
+  Tone.Transport.seconds = 0;
+  randomStartPosition = 0;
+  player.seek(0);
 }
 
 
