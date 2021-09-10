e78dc9532095ce316187d0877739e6b6f07dd2d3
pplzpplzpplz <moore.caleb@gmail.com>
2021-09-10 08:45:56 -0700

    cleanup

--- take_four/takefour_tone.js
+++ take_four/takefour_tone.js
@@ -7,7 +7,7 @@ let playCount1;
 let playCount2;
 
 // tone.js - new Buffer for audio file, to grab duration
-var buffer1 = new Tone.Buffer("1b.wav");
+var buffer1 = new Tone.Buffer("1c.wav");
 var buffer2 = new Tone.Buffer("1b.wav");
 // tone.js - new Player 
 const player1 = new Tone.Player(buffer1).toDestination();
@@ -47,6 +47,7 @@ function startIt() {
     player1.playbackRate = 1;
     player1.start();
     Tone.Transport.start();
+    
 
     // seek to the random start position
     player1.seek(randomStartPosition1);
@@ -66,7 +67,7 @@ function startIt() {
         playCount1 = playCount1 + 1;
         randomStartPosition1 = 0;
         currentPosition1 = 0;
-        // Tone.Transport.seconds = 0;
+        Tone.Transport.seconds = 0;
       }
 
       // move the line with the audio playback
@@ -87,53 +88,53 @@ function startIt() {
 
 
 
-    // AUDIO 2 --------------------------------
-    // pick a random start time within the duration of the audio file
-    randomStartPosition2 = Math.random() * (buffer2.duration);
+    // // AUDIO 2 --------------------------------
+    // // pick a random start time within the duration of the audio file
+    // randomStartPosition2 = Math.random() * (buffer2.duration);
     
-    player2.fadeIn = .1;
-    player2.fadeOut = .1;
-    player2.loop = true;
-    player2.playbackRate = 1;
-    player2.start();
-    Tone.Transport.start();
+    // player2.fadeIn = .1;
+    // player2.fadeOut = .1;
+    // player2.loop = true;
+    // player2.playbackRate = 1;
+    // player2.start();
+    // Tone.Transport.start();
 
-    // seek to the random start position
-    player2.seek(randomStartPosition2);
+    // // seek to the random start position
+    // player2.seek(randomStartPosition2);
 
-    let currentPosition2 = Tone.Transport.seconds + randomStartPosition2;
-    playCount2 = 1;
+    // let currentPosition2 = Tone.Transport.seconds + randomStartPosition2;
+    // playCount2 = 1;
 
 
-    setInterval(function() {
-      // song is less than one duration
-      if (currentPosition2 < buffer2.duration) {
-        currentPosition2 = Tone.Transport.seconds + randomStartPosition2;
-      } 
+    // setInterval(function() {
+    //   // song is less than one duration
+    //   if (currentPosition2 < buffer2.duration) {
+    //     currentPosition2 = Tone.Transport.seconds + randomStartPosition2;
+    //   } 
 
-      // song is more than one duration
-      else if (currentPosition2 >= buffer2.duration) {
-        playCount2 = playCount2 + 1;
-        randomStartPosition2 = 0;
-        currentPosition2 = 0;
-        // Tone.Transport.seconds = 0;
-      }
+    //   // song is more than one duration
+    //   else if (currentPosition2 >= buffer2.duration) {
+    //     playCount2 = playCount2 + 1;
+    //     randomStartPosition2 = 0;
+    //     currentPosition2 = 0;
+    //     // Tone.Transport.seconds = 0;
+    //   }
 
-      // move the line with the audio playback
-      p2.style.left = `${((currentPosition2 / buffer2.duration) * 100)}%`;
+    //   // move the line with the audio playback
+    //   p2.style.left = `${((currentPosition2 / buffer2.duration) * 100)}%`;
 
-      // debugDiv.innerHTML = `
-      // <strong><u>DEBUG</u></strong> <br>
-      // randomStartPosition2:  ${randomStartPosition2.toFixed(1)}
-      // <br> 
-      // buffer2.duration: ${buffer2.duration} <br>
-      // currentPosition2: ${Math.floor(currentPosition2)}
-      // <br>
-      // playCount2: ${playCount2}
-      // <br>
-      // `;
-    });
-    // AUDIO 2 ----------------END----------------
+    //   // debugDiv.innerHTML = `
+    //   // <strong><u>DEBUG</u></strong> <br>
+    //   // randomStartPosition2:  ${randomStartPosition2.toFixed(1)}
+    //   // <br> 
+    //   // buffer2.duration: ${buffer2.duration} <br>
+    //   // currentPosition2: ${Math.floor(currentPosition2)}
+    //   // <br>
+    //   // playCount2: ${playCount2}
+    //   // <br>
+    //   // `;
+    // });
+    // // AUDIO 2 ----------------END----------------
 
 
 
@@ -149,5 +150,6 @@ function stopIt() {
   randomStartPosition1 = 0;
   player1.seek(0);
 }
+
 
 
